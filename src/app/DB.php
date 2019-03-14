52913998e4a4e3d56e7a1e0e6adac3ac5cf47de9
amaurygirard <amaury.girard@yahoo.fr>
2019-03-14 14:03:12 +0100

    Added two static methods to create database tables if they don't exist yet. This needs to be tested though...

--- src/app/DB.php
+++ src/app/DB.php
@@ -51,4 +51,84 @@
 
     }
 
+    /* Méthode pour créer les tables en BDD */
+    public static function tablesExist() {
+
+      // Create connection
+      $connexion = new mysqli(self::DB_SERVERNAME, self::DB_USERNAME, self::DB_PASSWD, self::DB_NAME);
+      // Check connection
+      if ($connexion->connect_error) {
+          die("Erreur de connexion à la base de données : " . $connexion->connect_error);
+      }
+
+      $return = (
+        $connexion->query("SHOW TABLES LIKE 'question'")
+        && $connexion->query("SHOW TABLES LIKE 'quizz'")
+        && $connexion->query("SHOW TABLES LIKE 'reponse'")
+      ) ? true : false;
+
+      $connexion->close();
+
+      return $return;
+
+    }
+
+    /* Méthode pour créer les tables en BDD */
+    public static function createTables() {
+
+      // Create connection
+      $connexion = new mysqli(self::DB_SERVERNAME, self::DB_USERNAME, self::DB_PASSWD, self::DB_NAME);
+      // Check connection
+      if ($connexion->connect_error) {
+          die("Erreur de connexion à la base de données : " . $connexion->connect_error);
+      }
+
+      // Création table Question
+      $sql = "CREATE TABLE IF NOT EXISTS `question` (
+        `id` int(55) NOT NULL AUTO_INCREMENT,
+        `quizz_id` int(20) NOT NULL,
+        `text` text COLLATE utf8_unicode_ci NOT NULL,
+        PRIMARY KEY (`id`),
+        KEY `quizz_id` (`quizz_id`)
+      ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;";
+
+      if ($connexion->query($sql) === TRUE) {
+          echo "Table `question` created successfully";
+      } else {
+          echo "Erreur lors de la création de la table `question` : " . $connexion->error;
+      }
+
+      // sql to create table
+      $sql = "CREATE TABLE IF NOT EXISTS `quizz` (
+        `id` tinyint(5) NOT NULL AUTO_INCREMENT,
+        `title` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+        PRIMARY KEY (`id`)
+      ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;";
+
+      if ($connexion->query($sql) === TRUE) {
+          echo "Table `quizz` created successfully";
+      } else {
+          echo "Erreur lors de la création de la table `quizz` : " . $connexion->error;
+      }
+
+      // sql to create table
+      $sql = "CREATE TABLE IF NOT EXISTS `reponse` (
+        `id` int(55) NOT NULL AUTO_INCREMENT,
+        `question_id` int(55) NOT NULL,
+        `text` text COLLATE utf8_unicode_ci NOT NULL,
+        `correct` tinyint(1) NOT NULL,
+        PRIMARY KEY (`id`),
+        KEY `question_id` (`question_id`)
+      ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;";
+
+      if ($connexion->query($sql) === TRUE) {
+          echo "Table `reponse` created successfully";
+      } else {
+          echo "Erreur lors de la création de la table `reponse` : " . $connexion->error;
+      }
+
+      $connexion->close();
+
+    }
+
   }
